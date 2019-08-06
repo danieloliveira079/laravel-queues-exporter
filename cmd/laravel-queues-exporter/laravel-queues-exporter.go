b5953f6f987b9906987ef3497dade84e5c5faa75
Daniel Oliveira <daniel.oliveira079@gmail.com>
2019-08-06 14:16:44 +0200

    Add RedisExporterBuilder to be used by main

--- cmd/laravel-queues-exporter/laravel-queues-exporter.go
+++ cmd/laravel-queues-exporter/laravel-queues-exporter.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"github.com/danieloliveira079/laravel-queues-exporter/pkg/config"
 	"github.com/danieloliveira079/laravel-queues-exporter/pkg/exporter/redis"
 	"github.com/danieloliveira079/laravel-queues-exporter/pkg/forwarder"
 	"github.com/danieloliveira079/laravel-queues-exporter/pkg/metric"
@@ -15,48 +17,22 @@ type Forwarder interface {
 	Forward(metrics []metric.Metric)
 }
 
-//TODO Extract exporter and dependencies initialisation from main()
+type Exporter interface {
+	Run(collected chan []metric.Metric)
+}
+type ExporterBuilder interface {
+	Build(appConfig config.AppConfig) (Exporter, error)
+}
+
 func main() {
-	getConfig()
+	appConfig := getConfig()
 
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGHUP, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGINT)
 	done := make(chan os.Signal, 1)
 
-	connectionConfig := redis.ConnectionConfig{
-		Host: config.redisHost,
-		Port: config.redisPort,
-		DB:   config.redisDB,
-	}
-
-	connectorConfig := redis.ConnectorConfig{
-		ConnConfig: connectionConfig,
-	}
-	connector, err := redis.NewRedisConnector(connectorConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	dispatcher, err := redis.NewRedisCommandDispatcher(connector)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	extractorConfig := redis.ExtractorConfig{
-		Dispatcher: dispatcher,
-	}
-	extractor, err := redis.NewRedisExtractor(extractorConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	exporterConfig := redis.ExporterConfig{
-		ConnectionConfig: connectionConfig,
-		QueueNames:       config.queuesNames,
-		CollectInterval:  config.collectInterval,
-	}
-	exporter, err := redis.NewRedisExporter(exporterConfig, connector, extractor)
-
+	redisExporterBuilder := new(redis.RedisExporterBuilder)
+	exporter, err := redisExporterBuilder.Build(appConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,6 +48,7 @@ func main() {
 	for {
 		select {
 		case metrics := <-collected:
+			//TODO abstract forwarder and use concurrency
 			for _, f := range forwarders {
 				f.Forward(metrics)
 			}
@@ -86,4 +63,20 @@ func main() {
 			}
 		}
 	}
+}
+
+func getConfig() config.AppConfig {
+	var appConfig config.AppConfig
+
+	flag.StringVar(&appConfig.RedisHost, "redis-host", config.GetEnv("REDIS_HOST", "127.0.0.1"), "Redis host where queues are stored")
+	flag.StringVar(&appConfig.RedisPort, "redis-port", config.GetEnv("REDIS_PORT", "6379"), "Redis target port open for connections")
+	flag.IntVar(&appConfig.RedisDB, "redis-db", config.GetEnvInt("REDIS_DB", 0), "Redis DB used by Laravel")
+	flag.StringVar(&appConfig.StatsDHost, "statsd-host", config.GetEnv("STATSD_HOST", "127.0.0.1"), "StatsD target to where metrics must be sent")
+	flag.StringVar(&appConfig.StatsDPort, "statsd-port", config.GetEnv("STATSD_PORT", "8125"), "StatsD target port open for connections")
+	flag.StringVar(&appConfig.MetricsPrefix, "metrics-prefix", config.GetEnv("METRICS_PREFIX", "exporter"), "Prefix to be added to every metric")
+	flag.IntVar(&appConfig.CollectInterval, "collect-interval", config.GetEnvInt("SCAN_INTERVAL", 60), "Interval in seconds between each metrics collect")
+	flag.StringVar(&appConfig.QueuesNames, "queues-names", config.GetEnv("QUEUES_NAMES", ""), "Names of the queues to be scanned separated by comma. I.e: queue1,queue2")
+
+	flag.Parse()
+	return appConfig
 }
