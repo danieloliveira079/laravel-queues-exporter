a2108a6a0c82db486cc081c354143a57dc685a1b
Daniel Oliveira <daniel.oliveira079@gmail.com>
2019-08-13 17:04:21 +0200

    Add helper methods to Metric

--- pkg/metric/metric.go
+++ pkg/metric/metric.go
@@ -1,6 +1,20 @@
 package metric
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Metric struct {
 	Name  string
 	Value int64
+}
+
+func (m *Metric) WithPrefix(prefix string) string {
+	parsedName := strings.ReplaceAll(m.Name, ":", ".")
+	return fmt.Sprintf("%s.%s", prefix, parsedName)
+}
+
+func (m *Metric) ValueToFloat64() float64 {
+	return float64(m.Value)
 }
